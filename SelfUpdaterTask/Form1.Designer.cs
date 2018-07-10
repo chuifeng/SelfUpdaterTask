860fdba8edc1a00a971ceba11f538157de7e2ac7
Bulat-N <bulat@nuriev.me>
2018-07-11 02:50:50 +0300

    Добавлены файлы проекта.

---
+++ SelfUpdaterTask/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SelfUpdaterTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textBoxVersion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(12, 9);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(44, 13);
+            this.labelVersion.TabIndex = 0;
+            this.labelVersion.Text = "Версия";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(15, 32);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(258, 23);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "Обновить";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textBoxVersion
+            // 
+            this.textBoxVersion.Location = new System.Drawing.Point(62, 6);
+            this.textBoxVersion.Name = "textBoxVersion";
+            this.textBoxVersion.ReadOnly = true;
+            this.textBoxVersion.Size = new System.Drawing.Size(211, 20);
+            this.textBoxVersion.TabIndex = 2;
+            this.textBoxVersion.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 67);
+            this.Controls.Add(this.textBoxVersion);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.labelVersion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Test task. Updater.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBoxVersion;
+    }
+}
+
---
+++ SelfUpdaterTask/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SelfUpdaterTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var CurrentVersion = FileVersionInfo.GetVersionInfo(Application.ExecutablePath).FileVersion;
+            var Args = Environment.GetCommandLineArgs();
+            foreach (var Argument in Args)
+                if (Argument == "ShowVersion")
+                    ShowVersionAndExit(CurrentVersion);
+            textBoxVersion.Text = CurrentVersion;
+        }
+
+        void ShowVersionAndExit(string CurrentVersion)
+        {
+            MessageBox.Show("Версия программы " + CurrentVersion);
+            Environment.Exit(1);
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var FilePath = Path.GetDirectoryName(Application.ExecutablePath);
+                var FileName = Path.GetFileName(Application.ExecutablePath);
+                var OldFile = Application.ExecutablePath + "_old";
+                File.Move(Application.ExecutablePath, OldFile);
+                File.Copy(String.Format("{0}/new/{1}", FilePath, FileName), Application.ExecutablePath);
+                Process.Start(new ProcessStartInfo()
+                {
+                    Arguments = String.Format("/c ping localhost & del {0} & start {1}", OldFile, Application.ExecutablePath),
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true,
+                    FileName = "cmd.exe"
+                });
+                Environment.Exit(1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
